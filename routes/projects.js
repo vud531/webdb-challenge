f09d20b26d81598c11cfafb31a364a0d793f9f8a
Duc Vu <de.yd315@gmail.com>
2019-05-03 11:04:57 -0500

    added get and post route for projects

--- routes/projects.js
+++ routes/projects.js
@@ -1,1 +1,19 @@
 const express = require('express')
+const asyncHandler = require('../helpers/asyncHandler')
+const db = require('../data/dbConfig')
+
+const router = express.Router()
+const table = 'projects'
+
+router.get('/', asyncHandler( async(req, res) => {
+    const result = await db(table)
+    res.status(200).json(result)
+}))
+
+router.post('/', asyncHandler( async(req, res) => {
+    const [id] = await DB.addDish(req.body);
+    const result = await db(model)
+    .where({ id })
+    .first();
+    res.status(201).json(result);
+}))
